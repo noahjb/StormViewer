e9c4b000fd5118c94e35b73d0145de8ce3ea34ba
Noah <nbalsmeyer@gmail.com>
2017-04-04 19:42:53 +0200

    Added Facebook and sharing button. Facebook button is currently disabled in favor of the general Facebook sharing.

--- StormViewer/DetailViewController.swift
+++ StormViewer/DetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Social
 
 class DetailViewController: UIViewController {
 
@@ -18,6 +19,7 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         
         title = selectedImage
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
 
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
@@ -32,6 +34,18 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    func shareTapped() {
+        //if let vc = SLComposeViewController(forServiceType: SLServiceTypeFacebook) {
+        //    vc.setInitialText("Look at this great picture!")
+        //    vc.add(imageView.image!)
+        //    vc.add(URL(string: "http://www.photolib.noaa.gov/nssl"))
+        //    present(vc, animated: true)
+        //}
+        let vc = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
